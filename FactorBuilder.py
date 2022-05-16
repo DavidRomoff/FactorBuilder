30e748b1dbba0df24b48b95e40323e1645da9ffe
David Romoff <74327064+DavidRomoff@users.noreply.github.com>
2022-05-16 17:01:38 -0400

    Add files via upload

--- FactorBuilder.py
+++ FactorBuilder.py
@@ -11,21 +11,38 @@ bookValue = np.random.normal(80,10,n)
 returns = np.random.normal(.01,.005,n)
 df = pd.DataFrame(data=zip(returns,marketCap,bookValue),columns=['returns','marketCap','bookValue'],index=range(1,n+1))
 
-def CategoryBuilder(X: pd.DataFrame,category_colname: str,cutoffs: str,labels: str,
-                    categoryType = 'percentile',categoryTitle = 'categories') -> None: 
+def CategoryBuilder(X: pd.DataFrame,
+                    category_colname: str,
+                    cutoffs: str,
+                    labels: str,
+                    categoryType = 'percentile',
+                    categoryTitle = 'categories') -> None: 
+
+    X[categoryTitle] = 'unlabeled'
+    
     if categoryType == 'value':
-        X[categoryTitle] = 'unlabeled'
         lastcutoff = math.inf
         for cutoff,label in zip(cutoffs,labels):
             ind_category = np.logical_and(X[category_colname] > cutoff, X[category_colname] <= lastcutoff)
             X.loc[ind_category,categoryTitle] = label
             lastcutoff = cutoff
 
-def FactorBuilder(X: pd.DataFrame,returns_colname: str,risk_colname: str,category_colname: str):
+    if categoryType == 'percentile':
+        pass
+
+def FactorBuilder(  X: pd.DataFrame,
+                    returns_colname: str,
+                    risk_colname: str,
+                    category_colname: str) -> float:
+
     ind_highLow = np.logical_or(X[risk_colname]=='high',X[risk_colname]=='low')
+
     X_aggregated = X.loc[ind_highLow,:].groupby([category_colname,risk_colname]).mean()
+
     categoryDiffMeans = X_aggregated.loc[:,'high',:][returns_colname] - X_aggregated.loc[:,'low',:][returns_colname]
+
     factor = categoryDiffMeans.mean()
+
     return factor
 
 CategoryBuilder(X=df,
